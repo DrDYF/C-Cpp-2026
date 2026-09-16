9d922c4e2a4a7db9d6720015085a72f7fe4c5fd8
Yifan Du <132108135+DrDYF@users.noreply.github.com>
2026-09-16 20:50:14 +0800

    Create main.cpp

---
+++ level1/p02_is_prime/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    bool isPrime = true;
+    cin>>n;
+    if(n<=1){
+        isPrime = false;
+    }
+    for(int i=2;i<=sqrt(n);i++){
+        if(n%i==0){
+            isPrime = false;
+            break;
+        }
+    }
+    if(isPrime){
+        cout << "是素数" << endl;
+    }else{
+        cout << "不是素数" << endl;
+    }
+    return 0;
+}
