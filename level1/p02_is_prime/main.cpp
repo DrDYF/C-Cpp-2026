51fad7d81ab6d497e74236858d144d6b41d14e63
DrDYF <132108135+DrDYF@users.noreply.github.com>
2026-09-25 21:36:21 +0800

    Update main.cpp

--- level1/p02_is_prime/main.cpp
+++ level1/p02_is_prime/main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
+
 using namespace std;
 
+// 判断 n 是不是素数。只要试到 sqrt(n) 就够了：
+// 如果 n 有大于 sqrt(n) 的因子，那它一定还有个小于 sqrt(n) 的搭档。
+bool isPrime(long long n) {
+    if (n < 2) return false;
+    for (long long i = 2; i * i <= n; ++i)
+        if (n % i == 0) return false;
+    return true;
+}
+
 int main() {
-    int n;
-    bool isPrime = true;
-    cin>>n;
-    if(n<=1){
-        isPrime = false;
+    long long n;
+    cout << "请输入一个正整数: ";
+    if (!(cin >> n)) {
+        cout << "输入无效\n";
+        return 1;
     }
-    for(int i=2;i<=sqrt(n);i++){
-        if(n%i==0){
-            isPrime = false;
-            break;
-        }
-    }
-    if(isPrime){
-        cout << "是素数" << endl;
-    }else{
-        cout << "不是素数" << endl;
-    }
+
+    cout << n << (isPrime(n) ? " 是素数" : " 不是素数") << '\n';
     return 0;
 }
